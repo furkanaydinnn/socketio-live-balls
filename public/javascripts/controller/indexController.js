5410692aa3603af2198daea38f96794805a696eb
Furkan Aydın <furkanaydin@Furkans-MacBook-Pro.local>
2020-04-11 12:12:05 +0300

    newUser saving

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,10 +1,23 @@
 app.controller('indexController',['$scope','indexFactory',($scope,indexFactory)=>{
-   indexFactory.connectSocket('http://localhost:3000',{
-      reconnectionAttemps:3,
-      reconnectionDelay:700
-   }).then((socket)=>{
-      console.log('connection success',socket);
-   }).catch((err)=>{
-      console.log(err);
-   })
+
+   $scope.init = ()=>{
+      const username = prompt('Please enter a username');
+      if(username)
+         initSocket(username);
+      else
+         return false;
+   }
+
+   function initSocket(username) {
+      indexFactory.connectSocket('http://localhost:3000',{
+         reconnectionAttemps:3,
+         reconnectionDelay:700
+      }).then((socket)=>{
+         socket.emit('newUser',{username});
+        //console.log('connection success',socket);
+      }).catch((err)=>{
+         console.log(err);
+      });
+   }
+
 }]);
